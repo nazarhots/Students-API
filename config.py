afacc5abaf80d371f4e09b9a8516227780b21425
Nazar Hots <gotsjob@gmail.com>
2023-06-13 19:03:00 +0300

    Add APP_SERVER_NAME to .env

--- config.py
+++ config.py
@@ -6,12 +6,14 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # DB data
-dbname=os.getenv("DB_NAME")
-user=os.getenv("DB_USER")
-password=os.getenv("DB_PASSWORD")
-host=os.getenv("DB_HOST")
-port=os.getenv("DB_PORT")
+dbname = os.getenv("DB_NAME")
+user = os.getenv("DB_USER")
+password = os.getenv("DB_PASSWORD")
+host = os.getenv("DB_HOST")
+port = os.getenv("DB_PORT")
 
 # Paths
 create_table_path = os.getenv("CREATE_TABLE_PATH")
 
+# Config
+app_server_name = os.getenv("APP_SERVER_NAME")
