524513c30ccf4988c462aeb582e6588b4e9610fe
Nazar Hots <gotsjob@gmail.com>
2023-06-15 16:33:34 +0300

    Add start api from main file

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from db_utils import execute_sql_file, generate_db_data
 from config import create_table_path
+from app import app
 
 
 if __name__ == "__main__":
     execute_sql_file(create_table_path)
     generate_db_data()
+    app.run()
